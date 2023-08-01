3263099ec64b0da33ab177ea38efd4cebf122712
SiuHangChan <schan247@my.centennialcollege.ca>
2023-08-01 09:05:46 -0400

    first upload

---
+++ app/config/auth.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    secret: "Sean-secret-key",
+  
+    jwtExpiration: 3600,           // 1 hour
+
+  jwtRefreshExpiration: 86400,   // 24 hours
+
+  /* for test */
+
+  jwtExpiration: 60,          // 1 minute
+
+  // jwtRefreshExpiration: 120,  // 2 minutes
+  };
---
+++ app/config/db.config.js
@@ -0,0 +1,4 @@
+module.exports={
+  
+    url:"mongodb+srv://schan247:1234Qwer@cluster0.akbrjrn.mongodb.net/SportStore?retryWrites=true&w=majority"
+}
